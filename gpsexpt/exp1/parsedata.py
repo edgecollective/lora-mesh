510fe698d35fd792f1b39592ac33c781c62b6e19
Don Blair <donblair@gmail.com>
2020-09-10 23:40:43 -0400

    update

--- gpsexpt/exp1/parsedata.py
+++ gpsexpt/exp1/parsedata.py
@@ -1,6 +1,10 @@
 f = open("trial2.txt", "r")
 lastroute=0
 lastrssi=0
+linecount=0
+lat='0'
+lon='0'
+print('[')
 for x in f:
   #print(x)
   s=x.split("&")
@@ -17,6 +21,8 @@ for x in f:
           #print('n3:',aq[4].split(':')[1])
           #print('r3:',aq[5].split(':')[1].strip('}]\r\n'))
           lastrssi=aq[5].split(':')[1].strip('}]\r\n')
+          if(lat!='0'):
+              print('{\"time\":\"'+time+'\",\"lastroute\":\"'+lastroute+'\",\"lastrssi\":\"'+lastrssi+'\",\"lon\":\"'+lon+'\",\"lat\":\"'+lat+'\"},')
       else:
           a=s[1]
           #print('GPS: ', a)
@@ -29,6 +35,9 @@ for x in f:
               #print(lat)
               lon=coord[1]
               #print("GPS -- lat,lon:",lat,lon)
-              print({'time':time,'lat':lat,'lon':lon,'lastroute':lastroute,'lastrssi':lastrssi})
+              #print('{\'time\':'+time+', \'lat\':'+lat+'lon':lon,'lastroute':lastroute,'lastrssi':lastrssi)
+              #print('{\"time\":\"'+time+'\",\"lastroute\":\"'+lastroute+'\",\"lastrssi\":\"'+lastrssi+'\",\"lon\":\"'+lon+'\",\"lat\":\"'+lat+'\"},')
+              #print(',')
           #lat=float(coord[0])
           #lon=float(coord[1])
+print(']')
