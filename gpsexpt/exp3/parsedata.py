7005efc3296cd2468308f8609f855cb75154d5be
Don Blair <donblair@gmail.com>
2020-09-12 15:22:01 -0400

    update

--- gpsexpt/exp3/parsedata.py
+++ gpsexpt/exp3/parsedata.py
@@ -1,4 +1,4 @@
-f = open("trial2.txt", "r")
+f = open("mesh.txt", "r")
 lastroute=0
 lastrssi=0
 linecount=0
@@ -9,35 +9,20 @@ for x in f:
   #print(x)
   s=x.split("&")
   time=s[0]
-  q=s[1].split(":")
-  #print(q)
-  if(len(q)>1):
-      #print(q)
-      if(q[0]=='routing'):
-          a=s[1]
-          #print('ROUTE: ',a)
-          aq=a.split('[',)[1].split(',')
-          lastroute=aq[4].split(':')[1]
-          #print('n3:',aq[4].split(':')[1])
-          #print('r3:',aq[5].split(':')[1].strip('}]\r\n'))
-          lastrssi=aq[5].split(':')[1].strip('}]\r\n')
-          if(lat!='0'):
-              print('{\"time\":\"'+time+'\",\"lastroute\":\"'+lastroute+'\",\"lastrssi\":\"'+lastrssi+'\",\"lon\":\"'+lon+'\",\"lat\":\"'+lat+'\"},')
-      else:
-          a=s[1]
-          #print('GPS: ', a)
-          #print(len(a.split(',')))
-          if(len(a.split(','))==2):
-          #if(a.split(':')[0]=='node'):
-              coord=a.split('[')[1].split(']')[0].split(',')
-              #print(coord)
-              lat=coord[0]
-              #print(lat)
-              lon=coord[1]
-              #print("GPS -- lat,lon:",lat,lon)
-              #print('{\'time\':'+time+', \'lat\':'+lat+'lon':lon,'lastroute':lastroute,'lastrssi':lastrssi)
-              #print('{\"time\":\"'+time+'\",\"lastroute\":\"'+lastroute+'\",\"lastrssi\":\"'+lastrssi+'\",\"lon\":\"'+lon+'\",\"lat\":\"'+lat+'\"},')
-              #print(',')
-          #lat=float(coord[0])
-          #lon=float(coord[1])
+  q=s[1].split(";")
+  #print(len(q))
+  if len(q)==3:
+    node=q[0].strip('[').strip(']').split(':')[1]
+    gps=q[1].strip('[').strip(']').split(',')
+    lat=gps[0]
+    lon=gps[1]
+    route=q[2].strip('[').strip(']').strip('\r').strip('\n').strip()
+    #print(node)
+    #print(lat,lon)
+    node1stats=route.split('}')[0].strip('{').split(',')
+    lastroute=node1stats[0].split(':')[1]
+    lastrssi=node1stats[1].split(':')[1]
+    #print(node1stats)
+    #print(relaynode,node1rssi)
+    print('{\"time\":\"'+time+'\",\"node\":\"'+node+'\",\"lastroute\":\"'+lastroute+'\",\"lastrssi\":\"'+lastrssi+'\",\"lon\":\"'+lon+'\",\"lat\":\"'+lat+'\"},')
 print(']')
