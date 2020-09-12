401551056cd39b9dfb1fc4a71628bbc63d34ad8d
Don Blair <donblair@gmail.com>
2020-09-12 12:59:37 -0400

    update

---
+++ gpsexpt/exp3/tinygps_featherm0_gpssend_node2/tinygps_featherm0_gpssend_node2.ino
@@ -0,0 +1,264 @@
+
+//#include <EEPROM.h>
+#include <RHRouter.h>
+#include <RHMesh.h>
+#include <RH_RF95.h>
+#define RH_HAVE_SERIAL
+#define LED 13
+#define N_NODES 3
+
+#include <TinyGPS++.h> //https://github.com/mikalhart/TinyGPSPlus
+
+// board manager for adafruit: https://adafruit.github.io/arduino-board-index/package_adafruit_index.json
+
+int targetNode = 1;
+
+int intervalGPS = 1000;
+
+static const uint32_t GPSBaud = 9600;
+
+    unsigned long nextGPS = millis() + intervalGPS + random(1000, 3000);
+
+TinyGPSPlus gps;
+
+int delaySec = 3; 
+uint8_t nodeId = 2; // change this for each node
+
+uint8_t routes[N_NODES]; // full routing table for mesh
+int16_t rssi[N_NODES]; // signal strength info
+
+// for feather m0  
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+// Singleton instance of the radio driver
+//RH_RF95 rf95;
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHMesh *manager;
+
+// message buffer
+char buf[RH_MESH_MAX_MESSAGE_LEN];
+
+double last_lon=0.;
+double last_lat=0.;
+
+void setup() {
+  randomSeed(analogRead(0));
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  //while (!Serial) ; // Wait for serial port to be available
+
+    Serial1.begin(GPSBaud);
+
+  
+  if (nodeId > 10) {
+    Serial.print(F("EEPROM nodeId invalid: "));
+    Serial.println(nodeId);
+    nodeId = 1;
+  }
+  Serial.print(F("initializing node "));
+
+  manager = new RHMesh(rf95, nodeId);
+
+  if (!manager->init()) {
+    Serial.println(F("init failed"));
+  } else {
+    Serial.println("done");
+  }
+  rf95.setTxPower(23, false);
+  rf95.setFrequency(915.0);
+  rf95.setCADTimeout(500);
+
+  // Possible configurations:
+  // Bw125Cr45Sf128 (the chip default)
+  // Bw500Cr45Sf128
+  // Bw31_25Cr48Sf512
+  // Bw125Cr48Sf4096
+
+  // long range configuration requires for on-air time
+  boolean longRange = false;
+  if (longRange) {
+    RH_RF95::ModemConfig modem_config = {
+      0x78, // Reg 0x1D: BW=125kHz, Coding=4/8, Header=explicit
+      0xC4, // Reg 0x1E: Spread=4096chips/symbol, CRC=enable
+      0x08  // Reg 0x26: LowDataRate=On, Agc=Off.  0x0C is LowDataRate=ON, ACG=ON
+    };
+    rf95.setModemRegisters(&modem_config);
+    if (!rf95.setModemConfig(RH_RF95::Bw125Cr48Sf4096)) {
+      Serial.println(F("set config failed"));
+    }
+  }
+
+  Serial.println("RF95 ready");
+
+  for(uint8_t n=1;n<=N_NODES;n++) {
+    routes[n-1] = 0;
+    rssi[n-1] = 0;
+  }
+
+  Serial.print(F("mem = "));
+//  Serial.println(freeMem());
+
+}
+
+const __FlashStringHelper* getErrorString(uint8_t error) {
+  switch(error) {
+    case 1: return F("invalid length");
+    break;
+    case 2: return F("no route");
+    break;
+    case 3: return F("timeout");
+    break;
+    case 4: return F("no reply");
+    break;
+    case 5: return F("unable to deliver");
+    break;
+  }
+  return F("unknown");
+}
+
+void updateRoutingTable() {
+  for(uint8_t n=1;n<=N_NODES;n++) {
+    RHRouter::RoutingTableEntry *route = manager->getRouteTo(n);
+    if (n == nodeId) {
+      routes[n-1] = 255; // self
+    } else {
+      routes[n-1] = route->next_hop;
+      if (routes[n-1] == 0) {
+        // if we have no route to the node, reset the received signal strength
+        rssi[n-1] = 0;
+      }
+    }
+  }
+}
+
+// Create a JSON string with the routing info to each node
+void getRouteInfoString(char *p, size_t len) {
+  p[0] = '\0';
+  strcat(p, "[");
+  for(uint8_t n=1;n<=N_NODES;n++) {
+    strcat(p, "{\"n\":");
+    sprintf(p+strlen(p), "%d", routes[n-1]);
+    strcat(p, ",");
+    strcat(p, "\"r\":");
+    sprintf(p+strlen(p), "%d", rssi[n-1]);
+    strcat(p, "}");
+    if (n<N_NODES) {
+      strcat(p, ",");
+    }
+  }
+  strcat(p, "]");
+}
+
+void getGPSInfoString(char *p, size_t len) {
+
+String lon_value = String(last_lon,6);
+String lat_value = String(last_lat,6);
+
+sprintf(p, "[%s,%s];[",lat_value.c_str(),lon_value.c_str());
+for(uint8_t n=1;n<=N_NODES;n++) {
+   strcat(p, "{\"n\":");
+    sprintf(p+strlen(p), "%d", routes[n-1]);
+    strcat(p, ",");
+    strcat(p, "\"r\":");
+    sprintf(p+strlen(p), "%d", rssi[n-1]);
+    strcat(p, "}");
+     if (n<N_NODES) {
+      strcat(p, ",");
+    }
+}
+strcat(p, "]");
+}
+
+void printNodeInfo(uint8_t node, char *s) {
+  Serial.print(F("node: "));
+  Serial.print(F("{"));
+  Serial.print(F("\""));
+  Serial.print(node);
+  Serial.print(F("\""));
+  Serial.print(F(": "));
+  Serial.print(s);
+  Serial.println(F("}"));
+}
+
+int gpsreadcount=0;
+
+void loop() {
+
+int n = targetNode;
+
+// listen every loop for incoming messages so we can relay them:
+
+
+
+if (nextGPS <= millis()) { // time to make a GPS measurement and send it
+  
+while (Serial1.available() > 0)
+  {
+    gps.encode(Serial1.read());
+    if (gps.location.isValid())
+    {
+      last_lon=double(gps.location.lng());
+      last_lat=double(gps.location.lat());
+    } 
+    gpsreadcount++;
+  }
+     nextGPS = millis() + intervalGPS + random(1000, 5000);
+
+     /*broadcast*/
+     
+     Serial.print(last_lat,6);
+     Serial.print(",");
+     Serial.println(last_lon,6);
+
+     Serial.print('gpsreadcount=');
+  Serial.println(gpsreadcount);
+  gpsreadcount=0;
+
+  // grab last location values and send over radio
+  Serial.print(F("(lat,lon): "));
+  Serial.print(last_lat,6);
+  Serial.print(F(","));
+  Serial.println(last_lon,6);
+
+      updateRoutingTable();
+
+    //send GPS
+    getGPSInfoString(buf, RH_MESH_MAX_MESSAGE_LEN);
+    Serial.print(F("->"));
+    Serial.print(n);
+    Serial.print(F(" :"));
+    Serial.print(buf);
+
+    // send an acknowledged message to the target node
+    uint8_t error = manager->sendtoWait((uint8_t *)buf, strlen(buf), n);
+    if (error != RH_ROUTER_ERROR_NONE) {
+      Serial.println();
+      Serial.print(F(" ! "));
+      Serial.println(getErrorString(error));
+    } else {
+      Serial.println(F(" OK"));
+      // we received an acknowledgement from the next hop for the node we tried to send to.
+      RHRouter::RoutingTableEntry *route = manager->getRouteTo(n);
+      if (route->next_hop != 0) {
+        Serial.print("next hop:");
+        Serial.println(route->next_hop);
+        for (int i=0;i<route->next_hop;i++) { // blink corresponding to which node we hopped to
+          digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+          delay(100);                       // wait for a second
+          digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+          delay(100); 
+        }
+        rssi[route->next_hop-1] = rf95.lastRssi();
+      }
+    }
+  //delay(delaySec*1000);
+  
+}
+
+ 
+}
