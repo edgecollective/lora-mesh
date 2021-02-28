317f5c478137d025c9898b80dc9493ef647ddbf8
Don Blair <donblair@gmail.com>
2021-02-27 19:52:26 -0500

    update

---
+++ co2/simple_a/remote_node_heltec_2/remote_node_heltec_2.ino
@@ -0,0 +1,127 @@
+
+#include <RHSoftwareSPI.h> 
+#include <RHRouter.h>
+#include <RHMesh.h>
+#include <RH_RF95.h>
+#define RH_HAVE_SERIAL
+#include <SPI.h>
+
+#define nodeId 2
+
+#define N_NODES  4
+#define targetNode 1
+#define LED 25
+
+// heltec wifi lora 32 v2
+#define RFM95_CS 18
+#define RFM95_RST 14
+#define RFM95_INT 26
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHMesh *manager;
+
+// message buffer
+char buf[RH_MESH_MAX_MESSAGE_LEN];
+
+void setup() {
+
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial) ; // Wait for serial port to be available
+
+
+  Serial.print(F("initializing node "));
+
+  manager = new RHMesh(rf95, nodeId);
+
+  if (!manager->init()) {
+    Serial.println(F("init failed"));
+  } else {
+    Serial.println("done");
+  }
+  rf95.setTxPower(23, false);
+  rf95.setFrequency(915.0);
+  rf95.setCADTimeout(500);
+
+  // Possible configurations:
+  // Bw125Cr45Sf128 (the chip default)
+  // Bw500Cr45Sf128
+  // Bw31_25Cr48Sf512
+  // Bw125Cr48Sf4096
+
+  // long range configuration requires for on-air time
+  boolean longRange = false;
+  if (longRange) {
+    RH_RF95::ModemConfig modem_config = {
+      0x78, // Reg 0x1D: BW=125kHz, Coding=4/8, Header=explicit
+      0xC4, // Reg 0x1E: Spread=4096chips/symbol, CRC=enable
+      0x08  // Reg 0x26: LowDataRate=On, Agc=Off.  0x0C is LowDataRate=ON, ACG=ON
+    };
+    rf95.setModemRegisters(&modem_config);
+    if (!rf95.setModemConfig(RH_RF95::Bw125Cr48Sf4096)) {
+      Serial.println(F("set config failed"));
+    }
+  }
+
+  Serial.println("RF95 ready");
+
+}
+
+const __FlashStringHelper* getErrorString(uint8_t error) {
+  switch(error) {
+    case 1: return F("invalid length");
+    break;
+    case 2: return F("no route");
+    break;
+    case 3: return F("timeout");
+    break;
+    case 4: return F("no reply");
+    break;
+    case 5: return F("unable to deliver");
+    break;
+  }
+  return F("unknown");
+}
+
+void loop() {
+
+    if (nodeId != targetNode) {
+      
+    // send an acknowledged message to the target node
+    uint8_t error = manager->sendtoWait((uint8_t *)buf, strlen(buf), targetNode);
+    if (error != RH_ROUTER_ERROR_NONE) {
+      Serial.println();
+      Serial.print(F(" ! "));
+      Serial.println(getErrorString(error));
+    } else {
+      Serial.println(F(" OK"));
+    
+    }
+    }
+
+    // listen for incoming messages. Wait a random amount of time before we transmit
+    // again to the next node
+    unsigned long nextTransmit = millis() + random(3000, 5000);
+    while (nextTransmit > millis()) {
+      int waitTime = nextTransmit - millis();
+      uint8_t len = sizeof(buf);
+      uint8_t from;
+      if (manager->recvfromAckTimeout((uint8_t *)buf, &len, waitTime, &from)) {
+        buf[len] = '\0'; // null terminate string
+        Serial.print(from);
+        Serial.print(F("->"));
+        Serial.print(F(" :"));
+        Serial.println(buf);
+        
+      }
+    }
+
+  
+
+}
