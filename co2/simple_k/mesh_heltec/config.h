4b13cc82c68c26320ec148ba6703d02d1d40310d
Don Blair <donblair@gmail.com>
2021-03-03 15:44:52 -0500

    fixed bug in relaying the co2 code

---
+++ co2/simple_k/mesh_heltec/config.h
@@ -0,0 +1,29 @@
+
+#define this_node_id 1 // node #1 is the gateway
+const char* loranet_pubkey = "d5jahi4w8rvm"; // pub key for the gateway
+const char* this_node_pubkey = "d5jahi4w8rvm"; // pub key for this node (same as above if we're node #1 / gateway)
+const char* this_node_privkey = "56jpn44sw7fh"; // priv key for this node
+
+const char *SSID = "Fios-7S5yS";
+const char *WiFiPassword = "whose44gel62has";
+//const char* bayou_base_url = "192.168.1.163:3000/data";
+const char* bayou_base_url = "co2data.pvos.org/data";
+const int interval_sec = 5;
+const int random_interval_sec = 3; // range of random seconds to add to interval_sec
+const int forcePPM = 400;
+
+
+/*
+#define this_node_id 4 // node #1 is the gateway
+const char* loranet_pubkey = "d5jahi4w8rvm"; // pub key for the gateway
+const char* this_node_pubkey = "mkxgy4j2afer"; // pub key for this node (same as above if we're node #1 / gateway)
+const char* this_node_privkey = "dz45ns9iftnc"; // priv key for this node
+
+const char *SSID = "Fios-7S5yS";
+const char *WiFiPassword = "whose44gel62has";
+//const char* bayou_base_url = "192.168.1.163:3000/data";
+const char* bayou_base_url = "co2data.pvos.org/data";
+const int interval_sec = 5;
+const int random_interval_sec = 3; // range of random seconds to add to interval_sec
+const int forcePPM = 400;
+*/
