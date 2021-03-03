f5ed0d3f41d7df82e28a2cfcb79a812b7a8c2cac
Don Blair <donblair@gmail.com>
2021-03-03 09:35:21 -0500

    got display working; nice code refactoring

---
+++ co2/simple_j/mesh_heltec/config.h
@@ -0,0 +1,9 @@
+#define this_node_id 1
+const char* loranet_pubkey = "7zqjzktfq997"; // pub key for the gateway
+const char *SSID = "Fios-7S5yS";
+const char *WiFiPassword = "whose44gel62has";
+const char* bayou_base_url = "192.168.1.163:3000/data";
+const char* this_node_pubkey = "7zqjzktfq997"; 
+const char* this_node_privkey = "4g5xf5xzzhr8";
+const int interval_sec = 5;
+const int forcePPM = 400;
